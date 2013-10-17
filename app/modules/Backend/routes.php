2f0963b010144500cb3891a88e61f65ebeb6a02f
Lovro Papež <lovro.papez@gmail.com>
2013-10-17 15:46:22 +0100

    Add route group for the Backend.

--- app/modules/Backend/routes.php
+++ app/modules/Backend/routes.php
@@ -4,8 +4,11 @@ Route::get('login', 'Backend\Controllers\SessionsController@create');
 Route::get('logout', 'Backend\Controllers\SessionsController@destroy');
 Route::resource('sessions', 'Backend\Controllers\SessionsController', ['only' => ['create','store','destroy']]);
 
-Route::get('backend', function()
+Route::group(array('prefix' => 'backend', 'before' => 'auth'), function()
 {
-	var_dump(Auth::user()->email);
-	echo '<a href="/logout">Logout</a>';
-})->before('auth');
+	Route::get('/', function()
+	{
+		var_dump(Auth::user()->email);
+		echo '<a href="/logout">Logout</a>';
+	});
+});
