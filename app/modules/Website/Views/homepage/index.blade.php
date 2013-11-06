214ac81c9ccfd5e780ae8ef87f5bc75e62099e8a
Lovro Papež <lovro.papez@gmail.com>
2013-11-06 17:26:23 +0100

    Update featured to show only on first page.

--- app/modules/Website/Views/homepage/index.blade.php
+++ app/modules/Website/Views/homepage/index.blade.php
@@ -5,6 +5,7 @@
 
 		<div class="col-md-8 blog">
 
+			@if ($posts->getCurrentPage() == 1 and !Request::has('q'))
 				<section class="primary">
 					<div id="carousel-featured" class="carousel slide" data-ride="carousel">
 					  <!-- Indicators -->
@@ -36,37 +37,44 @@
 			        <span class="glyphicon glyphicon-chevron-right"></span>
 			      </a>
 				</section>
+			@else
+				@if (Request::has('q'))
+					{{$posts->appends(array('q' => Request::get('q')))->links()}}
+				@else
+					{{$posts->links()}}	
+				@endif		
+			@endif
 
-				@foreach($posts as $index => $post)
-					<section class="secondary">
-					  <div class="article-image">
-							<img src="{{display_post_image($post->photo, 'sm_')}}" class="thumbnail">
-							<a href="{{route('posts.show', $post->slug)}}">
-								<span>{{$post->title}}</span>
-							</a>
-						</div>
+			@foreach($posts as $index => $post)
+				<section class="secondary">
+				  <div class="article-image">
+						<img src="{{display_post_image($post->photo, 'sm_')}}" class="thumbnail">
+						<a href="{{route('posts.show', $post->slug)}}">
+							<span>{{$post->title}}</span>
+						</a>
+					</div>
 
-						<div class="article-info">
-					    <h2>
-					    	<a href="{{route('posts.show', $post->slug)}}">{{$post->title}}</a>
-					    </h2>
-					    <p>
-					    	<span style="font-weight: bold; color: #999">
-					    	Objavljeno: {{date('d.m.Y', strtotime($post->created_at))}} | 
-					    	Avtor: @if (!empty($post->author_alias)) {{$post->author_alias}} @else {{$post->post_author->username}} @endif
-					    	</span>
-								<br>
-					    	@if (!empty($post->summary) and !Str::contains($post->summary, '<img'))
-					    		{{$post->summary}}
-					    	@else
-									{{Str::limit(strip_tags($post->content), 150, '...')}}
-					    	@endif
+					<div class="article-info">
+				    <h2>
+				    	<a href="{{route('posts.show', $post->slug)}}">{{$post->title}}</a>
+				    </h2>
+				    <p>
+				    	<span style="font-weight: bold; color: #999">
+				    	Objavljeno: {{date('d.m.Y', strtotime($post->created_at))}} | 
+				    	Avtor: @if (!empty($post->author_alias)) {{$post->author_alias}} @else {{$post->post_author->username}} @endif
+				    	</span>
+							<br>
+				    	@if (!empty($post->summary) and !Str::contains($post->summary, '<img'))
+				    		{{$post->summary}}
+				    	@else
+								{{Str::limit(strip_tags($post->content), 150, '...')}}
+				    	@endif
 
-					    	<a href="{{route('posts.show', $post->slug)}}">Preberi več &raquo;</a>
-					    </p>
-						</div>
-					</section>
-				@endforeach
+				    	<a href="{{route('posts.show', $post->slug)}}">Preberi več &raquo;</a>
+				    </p>
+					</div>
+				</section>
+			@endforeach
 			@if (Request::has('q'))
 				{{$posts->appends(array('q' => Request::get('q')))->links()}}
 			@else
