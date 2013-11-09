83691a70bf244ebf15aa116b0f794a6e198b2c59
Lovro Papež <lovro.papez@gmail.com>
2013-11-09 11:32:29 +0100

    Fix on join module.

--- app/modules/Website/Views/homepage/join.blade.php
+++ app/modules/Website/Views/homepage/join.blade.php
@@ -1,9 +1,5 @@
 <h4>Pridruži se nam</h4>
 
-<a href="/novice/podaljsevanje-clanstva-in-vclanjevanje/">
-	Včlanjevanje v sezono 2013/2014 <span class="glyphicon glyphicon-hand-right"></span>	
-</a><br>
-
 <a href="/{{$supportersClub->slug}}/{{$becomeMember->slug}}">
 	Kako postati član <span class="glyphicon glyphicon-hand-right"></span>	
 </a><br>
