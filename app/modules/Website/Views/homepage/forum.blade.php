61b014b4209663bb9a59db08538620344a9d44ef
Lovro Papež <lovro.papez@gmail.com>
2013-11-16 13:32:47 +0100

    Update forum timestamps to only time.

--- app/modules/Website/Views/homepage/forum.blade.php
+++ app/modules/Website/Views/homepage/forum.blade.php
@@ -1,7 +1,7 @@
 <h4>Zadnje s foruma</h4>
 
 @foreach($forumPosts as $index => $post)
-	<strong>{{$post->member_name}}</strong> - {{$post->post_date->format('d.m.Y H:i:s')}} <br>
+	<strong>{{$post->member_name}}</strong> - {{$post->post_date->format('H:i')}} <br>
 	<a href="{{$post->link}}" target="_blank">
 		{{$post->title}}... <span class="glyphicon glyphicon-hand-right"></span>	
 	</a><br>
