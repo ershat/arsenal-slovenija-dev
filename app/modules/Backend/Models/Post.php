eaf667cc09ff77a0b296dadf8690ccc89f98bb25
Lovro Papež <lovro.papez@gmail.com>
2013-11-17 17:02:53 +0100

    Disable updating slugs for posts on update.

--- app/modules/Backend/Models/Post.php
+++ app/modules/Backend/Models/Post.php
@@ -19,7 +19,7 @@ class Post extends Eloquent {
 	public static $sluggable = array(
     'build_from' => array('title'),
     'save_to'    => 'slug',
-    'on_update'  => true,
+    // 'on_update'  => true,
     'include_trashed' => true
   );
 
