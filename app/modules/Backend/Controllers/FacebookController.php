9db28c0628e089abe3a71d477b869f5919ba639d
Lovro Papež <lovro.papez@gmail.com>
2013-12-09 20:30:19 +0100

    Init the auto Facebook post creation.

--- app/modules/Backend/Controllers/FacebookController.php
+++ app/modules/Backend/Controllers/FacebookController.php
@@ -31,6 +31,8 @@ class FacebookController extends \BaseController {
 
 		if (!empty($post->summary)) {
 			$params['description'] = $post->summary;
+		} else {
+			$params['description'] = \Str::limit($post->content, 100, '...');
 		}
 
 		if (!empty($post->photo)) {
