880c827b18e72b0a279d7b0eb38676d7ed4e011b
Lovro Papež <lovro.papez@gmail.com>
2013-12-09 23:21:23 +0100

    Update data for forum database.

--- app/config/database.php
+++ app/config/database.php
@@ -65,7 +65,7 @@ return array(
 
 		'arsenal.slovenija.forum' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => '192.168.33.12',
 			'database'  => 'arsenal-slovenija-forum',
 			'username'  => 'dbadmin',
 			'password'  => '0rehova.p0tica',
