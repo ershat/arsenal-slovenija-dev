3cdf9d5a93e021b7a8ec92761364b41088d6e377
Lovro Papež <lovro.papez@gmail.com>
2013-12-31 12:33:18 +0100

    Finished the frontend for matches.

--- app/modules/Website/Views/homepage/matches.blade.php
+++ app/modules/Website/Views/homepage/matches.blade.php
@@ -1,53 +1,57 @@
-<div class="result-widget">
-	<div class="left">
-  	<div class="team-logo">
-      <img src="/images/matches/{{$last->home_team_image}}" alt="" style="height: 86px;">
-    </div> <!-- End .team-logo -->
-    <div class="team-name">
-    	<h3>{{$last->home_team}}</h3>
-    </div> <!-- End .team-name -->
-  </div> <!-- End .left -->
+@if (isset($last))
+  <div class="result-widget">
+  	<div class="left">
+    	<div class="team-logo">
+        <img src="/images/matches/{{$last->home_team_image}}" alt="" style="height: 86px;">
+      </div> <!-- End .team-logo -->
+      <div class="team-name">
+      	<h3>{{$last->home_team}}</h3>
+      </div> <!-- End .team-name -->
+    </div> <!-- End .left -->
 
-  <div class="middle">
-  	<p>{{$last->home_goals}} - {{$last->away_goals}}</p>
-  </div> <!-- End .middle -->
+    <div class="middle">
+    	<p>{{$last->home_goals}} - {{$last->away_goals}}</p>
+    </div> <!-- End .middle -->
 
-  <div class="right">
-  	<div class="team-logo">
-      <img src="/images/matches/{{$last->away_team_image}}" alt="" style="height: 86px;">
-    </div> <!-- End .team-logo -->
-    <div class="team-name">
-    	<h3>{{$last->away_team}}</h3>
-    </div> <!-- End .team-name -->
-  </div> <!-- End .right -->
-</div>
+    <div class="right">
+    	<div class="team-logo">
+        <img src="/images/matches/{{$last->away_team_image}}" alt="" style="height: 86px;">
+      </div> <!-- End .team-logo -->
+      <div class="team-name">
+      	<h3>{{$last->away_team}}</h3>
+      </div> <!-- End .team-name -->
+    </div> <!-- End .right -->
+  </div>
+@endif
 
 <div class="clearfix"></div>
 
-<strong>{{date('d.m.Y', strtotime($next->time))}} ob {{date('H:i', strtotime($next->time))}}</strong><br>
+@if (isset($next))
+  <strong>{{date('d.m.Y', strtotime($next->time))}} ob {{date('H:i', strtotime($next->time))}}</strong><br>
 
-<div class="result-widget next">
-	<div class="left">
-  	<div class="team-logo">
-      <img src="/images/matches/{{$next->home_team_image}}" alt="" style="height: 86px;">
-    </div> <!-- End .team-logo -->
-    <div class="team-name">
-      <h3>{{$next->home_team}}</h3>
-    </div> <!-- End .team-name -->
-  </div> <!-- End .left -->
+  <div class="result-widget next">
+  	<div class="left">
+    	<div class="team-logo">
+        <img src="/images/matches/{{$next->home_team_image}}" alt="" style="height: 86px;">
+      </div> <!-- End .team-logo -->
+      <div class="team-name">
+        <h3>{{$next->home_team}}</h3>
+      </div> <!-- End .team-name -->
+    </div> <!-- End .left -->
 
-  <div class="middle">
-    <p>{{$next->home_goals}} - {{$next->away_goals}}</p>
-  </div> <!-- End .middle -->
+    <div class="middle">
+      <p>{{$next->home_goals}} - {{$next->away_goals}}</p>
+    </div> <!-- End .middle -->
 
-  <div class="right">
-  	<div class="team-logo">
-      <img src="/images/matches/{{$next->away_team_image}}" alt="" style="height: 86px;">
-    </div> <!-- End .team-logo -->
-    <div class="team-name">
-      <h3>{{$next->away_team}}</h3>
-    </div> <!-- End .team-name -->
-  </div> <!-- End .right -->
-</div>
+    <div class="right">
+    	<div class="team-logo">
+        <img src="/images/matches/{{$next->away_team_image}}" alt="" style="height: 86px;">
+      </div> <!-- End .team-logo -->
+      <div class="team-name">
+        <h3>{{$next->away_team}}</h3>
+      </div> <!-- End .team-name -->
+    </div> <!-- End .right -->
+  </div>
+@endif
 
 <div class="clearfix"></div>
